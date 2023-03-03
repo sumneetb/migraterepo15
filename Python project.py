f4664da8ab6f8087fc5955893e9fd5e980688179
sumneet <sumneet>
2023-03-03 10:55:40 +0000

    Added python file

--- Python project.py
+++ Python project.py
@@ -1,10 +1,27 @@
-# Python program to find Area of a circle 
-
-def findArea(r): 
-	PI = 3.142
-	return PI * (r*r); 
-
-# Driver method 
-print("Area is %.6f" % findArea(5)); 
-
-# This code is contributed by Chinmoy Lenka
+# Win Automation 
+# pip install ahk
+import ahk
+auto = ahk.AHK()
+# Mouse actions
+auto.mouse_move(100, 100)
+auto.mouse_click()
+auto.mouse_drag(100, 100, 200, 200)
+auto.mouse_wheel(1)
+auto.get_mouse_position()
+# Keyboard actions
+auto.key_press('p')
+auto.key_down('p')
+auto.key_up('p')
+auto.key_toggle('CapsLock', True)
+auto.type('Hello World')
+auto.key_press('Ctrl + Shift + P')
+# Window actions
+auto.run_script("chrome.exe")
+app  = auto.find_window(title="Google Chrome")
+app.activate()
+app.maximize()
+app.minimize()
+app.send("Python")
+app.kill()
+# Search on Screen
+auto.image_search("icon.png")
